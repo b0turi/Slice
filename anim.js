273c46f7c0816ec383fe930142a52539c7c58847
Kyle Thompson <Kyle.Thompson@ibm.com>
2018-06-13 17:01:58 -0500

    Started working on Cards

--- anim.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $(".slider").hover(function(){
-    $(this).animate({width: "290px"}, 200, "easeOutExpo");
-  }, function(){
-    $(this).animate({width: "78px"}, 200, "easeOutExpo");
-  });
-});
